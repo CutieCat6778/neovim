0cc61a55629f8325acd470353a6ccfe29cf5a00d
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2025-05-26 18:20:06 +0200

    [UPDATE]

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,35 @@
+return {
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		local formatting = null_ls.builtins.formatting -- to setup formatters
+		local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+
+		local sources = {
+			null_ls.builtins.formatting.goimports,
+			diagnostics.checkmake,
+			formatting.prettier.with({ filetypes = { "html", "json", "yaml", "markdown", "javascript" } }),
+			formatting.stylua,
+			formatting.shfmt.with({ args = { "-i", "4" } }),
+		}
+
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		null_ls.setup({
+			-- debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
+			sources = sources,
+			-- you can reuse a shared lspconfig on_attach callback here
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ async = false })
+						end,
+					})
+				end
+			end,
+		})
+	end,
+}
