30efac888f845451daa22e9d8844fe2ee247881d
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2026-02-14 02:37:28 +0100

    [UPDATE] fixed format when save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -13,20 +13,23 @@ return {
 				go = { "gofmt" },
 				javascript = { "prettier" },
 				typescript = { "prettier" },
-				elixir = { "mix" },
 			},
 			formatters = {
 				["clang-format"] = {
 					prepend_args = { "-style=file", "-fallback-style=LLVM" },
 				},
+				swiftformat = {
+					command = "swiftformat",
+					-- SwiftFormat expects --stdinpath only when reading from stdin
+					args = { "--quiet", "--stdinpath", "$FILENAME" },
+					stdin = true,
+				},
 			},
 			format_on_save = function(bufnr)
-				local ignore_filetypes = { "oil" }
-				if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+				if vim.bo[bufnr].filetype == "oil" then
 					return
 				end
-
-				return { timeout_ms = 5000, lsp_fallback = true }
+				return { timeout_ms = 500, lsp_fallback = true }
 			end,
 			log_level = vim.log.levels.ERROR,
 		})
