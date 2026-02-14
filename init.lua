c57fb8463c29cbc5728cc61c7b767e48f3d09645
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2026-02-14 02:26:34 +0100

    [UPDATE] removed bufferline

--- init.lua
+++ init.lua
@@ -5,8 +5,6 @@ vim.g.maplocalleader = " "
 
 require("core.keymaps")
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -16,18 +14,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
-
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 
 -- Disable statusline in Neovim (using vim-tpipeline to show in tmux instead)
 vim.o.showmode = false
--- lua/plugins/buffers.lua
+++ lua/plugins/buffers.lua
@@ -3,27 +3,4 @@ return {
 		"famiu/bufdelete.nvim",
 		cmd = { "Bdelete", "Bwipeout" },
 	},
-	{
-		"akinsho/bufferline.nvim",
-		version = "*",
-		event = "VeryLazy",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-			"famiu/bufdelete.nvim",
-		},
-		opts = {
-			options = {
-				mode = "buffers",
-				always_show_bufferline = true,
-				show_close_icon = false,
-				show_buffer_close_icons = false,
-				separator_style = "slant",
-				diagnostics = "nvim_lsp",
-			},
-		},
-		config = function(_, opts)
-			vim.o.showtabline = 0
-			require("bufferline").setup(opts)
-		end,
-	},
 }
